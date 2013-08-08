5f93683d7667c49cbb2d08fb09d04ea1ed20750d
John S <jstefanski@sandiego.edu>
2013-08-07 20:50:02 -0700

    All the exercises from chapters 1 - 3.

---
+++ 2 Fun with if-else statements/2 Fun with if-else statements/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_Fun_with_if_else_statements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+                MessageBox.Show("The box is not checked");
+            else
+                label1.BackColor = Color.Aqua;
+        }
+    }
+}
